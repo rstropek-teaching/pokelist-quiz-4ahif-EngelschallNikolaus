802ef5594f58090099cb3f70a634660a003f62df
EngelschallNikolaus <31962790+EngelschallNikolaus@users.noreply.github.com>
2017-11-30 07:37:02 +0100

    solution commit

--- app.js
+++ app.js
@@ -1,37 +1,65 @@
 "use strict";
 const pokemonList = document.getElementById('pokemons');
+const loadingLabel = document.getElementById('loading');
 let nextPokemon = '';
-let lastPokemon = '';
+let previousPokemon = '';
 window.onload = () => {
     getPokemon('https://pokeapi.co/api/v2/pokemon/');
 };
 $("#next").click(function () {
-    getPokemon(nextPokemon);
+    if (nextPokemon != null)
+        getPokemon(nextPokemon);
 });
 $("#previous").click(function () {
-    getPokemon(lastPokemon);
+    if (previousPokemon != null)
+        getPokemon(previousPokemon);
 });
 function getPokemon(url) {
+    if (loadingLabel != null)
+        loadingLabel.innerHTML = "Loading...";
     fetch(url).then(response => {
         response.json().then(pokelist => {
             let html = '';
             let id = 0;
             for (const pokemon of pokelist.results) {
-                html += `<li>${pokemon.name}<button onclick="showDetail('${pokemon.name}')">Details</button></li>`;
+                html +=
+                    `<tr>
+                    <td id="${pokemon.name}">
+                        ${pokemon.name}
+                    </td>
+                    <td>
+                        <button onclick="showDetail('${pokemon.name}')">Details</button>
+                    </td>
+                </tr>`;
             }
             nextPokemon = pokelist.next;
-            lastPokemon = pokelist.previous;
-            pokemonList.innerHTML = html;
+            previousPokemon = pokelist.previous;
+            if (pokemonList != null)
+                pokemonList.innerHTML = html;
+            if (loadingLabel != null)
+                loadingLabel.innerHTML = "";
         });
     });
 }
+//function to show the Details for a certain pokemon
 function showDetail(pokeName) {
-    // const name=document.getElementById('name');
-    // const weight=document.getElementById('weight');
-    // const picture=document.getElementById('picture');
+    //information is fetched from the pokeapi
     fetch('https://pokeapi.co/api/v2/pokemon/' + pokeName + '/').then(response => {
-        response.json().then(pokeList => {
-            alert("Name: " + pokeList.name + "\nWeight: " + pokeList.weight + `<img src='${pokeList.sprites.front_default}'/>`);
+        response.json().then(pokemon => {
+            //relevant data is saved into pre-made html-tags
+            let details = '';
+            details += document.getElementById('name').innerHTML = "Name: " + pokemon.name;
+            details += document.getElementById('picture').innerHTML = `Image: <img src='${pokemon.sprites.front_default}'/>`;
+            details += document.getElementById('weight').innerHTML = "Weight: " + pokemon.weight;
+            //going through all the abilities of a pokemon in a loop
+            let abilities = 'Abilities: <ul>';
+            for (const ability of pokemon.abilities) {
+                abilities += `<li>${ability.ability.name}</li>`;
+            }
+            abilities += '</ul>';
+            details += document.getElementById('abilities').innerHTML = abilities;
         });
     });
+    //make the pre-made html tags, which were invisible by now, visible
+    document.getElementById('details').style.display = "block";
 }
